6e8c27b276fbbc87d188c8210000d0b58700c85f
Sharifjon <sharifboymuminov@gmail.com>
2024-12-11 03:57:29 +0500

    start work

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -4,8 +4,18 @@ import 'package:tic_tac_toe/utils/app_colors.dart';
 import 'package:tic_tac_toe/utils/app_size.dart';
 import 'package:tic_tac_toe/utils/app_text_style.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<String> displayXO = ['', '', '', '', '', '', '', '', ''];
+
+  String winner = "";
+  List<int> winnerIndex = [];
 
   @override
   Widget build(BuildContext context) {
@@ -19,27 +29,47 @@ class HomeScreen extends StatelessWidget {
         right: false,
         child: Column(
           children: [
+            Text(
+              winner,
+              style: AppTextStyle.coinyRegular.copyWith(
+                fontSize: 48.sp,
+                color: AppColors.white,
+              ),
+            ),
             Expanded(
               child: GridView(
+                physics: const NeverScrollableScrollPhysics(),
+                padding: EdgeInsets.symmetric(horizontal: 15.we),
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 3,
                 ),
                 children: List.generate(
-                  9,
+                  displayXO.length,
                   (index) {
                     return Padding(
                       padding: EdgeInsets.all(5.we),
-                      child: DecoratedBox(
-                        decoration: BoxDecoration(
-                          color: AppColors.secondaryColor,
-                          borderRadius: BorderRadius.circular(20.r),
-                        ),
-                        child: Center(
-                          child: Text(
-                            "X",
-                            style: AppTextStyle.coinyRegular.copyWith(
-                              color: AppColors.primaryColor,
-                              fontSize: 65.sp,
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            displayXO[index] = "X";
+                            checkWinner();
+                          });
+                        },
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 200),
+                          decoration: BoxDecoration(
+                            color: winnerIndex.contains(index)
+                                ? AppColors.accentColor
+                                : AppColors.secondaryColor,
+                            borderRadius: BorderRadius.circular(20.r),
+                          ),
+                          child: Center(
+                            child: Text(
+                              displayXO[index],
+                              style: AppTextStyle.coinyRegular.copyWith(
+                                color: AppColors.primaryColor,
+                                fontSize: 65.sp,
+                              ),
                             ),
                           ),
                         ),
@@ -48,10 +78,41 @@ class HomeScreen extends StatelessWidget {
                   },
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
     );
+  }
+
+  void checkWinner() {
+    List<List<int>> winConditions = [
+      [0, 1, 2], // 1-qator
+      [3, 4, 5], // 2-qator
+      [6, 7, 8], // 3-qator
+      [0, 3, 6], // 1-ustun
+      [1, 4, 7], // 2-ustun
+      [2, 5, 8], // 3-ustun
+      [0, 4, 8], // Asosiy diagonal
+      [2, 4, 6], // Qarama-qarshi diagonal
+    ];
+
+    for (var condition in winConditions) {
+      String a = displayXO[condition[0]];
+      String b = displayXO[condition[1]];
+      String c = displayXO[condition[2]];
+      // debugPrint(condition.toString());
+      // debugPrint("a = $a");
+      // debugPrint("b = $b");
+      // debugPrint("c = $c");
+
+      if (a.isNotEmpty && a == b && b == c) {
+        debugPrint("Hello");
+        setState(() {
+          winner = a;
+          winnerIndex = condition;
+        });
+      }
+    }
   }
 }
